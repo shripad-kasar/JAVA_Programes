8e6c31822eb86daeaeccb220e6a4fb5124145b65
shripad-kasar <shripadkasar@gmail.com>
2025-01-28 12:22:54 +0530

    added student.java

---
+++ Student.java
@@ -0,0 +1,14 @@
+public class Student{
+  
+	int id;
+	String name;
+	public Student(int id,String name){
+		this.id=id;
+		this.name=name;
+	}
+
+	public static void main(String[] args){
+
+		Student s=new Student(101,"Shripad");
+	}
+}
